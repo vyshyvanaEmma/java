1da4c761251dcfd3d3b0e4910932cd395da2a042
Benncio09 <lorebencini13@outlook.com>
2025-01-31 13:23:42 +0300

    es.23

---
+++ es24/Lista.java
@@ -0,0 +1,108 @@
+package es24;
+
+public class Lista {
+    private Nodo testa;
+
+    public Lista() {
+        this.testa = null;
+    }
+
+    public boolean addWord(String parola){
+        if(cercaParola(parola)){
+            return false;
+        }
+        Nodo nuovoNodo = new Nodo(parola);
+        nuovoNodo.setNext(testa);
+        testa = nuovoNodo;
+        return true;
+    }
+    public boolean cercaParola(String p){
+        Nodo corrente = testa; 
+        while (corrente != null) {  
+            if (corrente.getInfo().equals(p)) {   
+                return true;  
+            }
+            corrente = corrente.getNext();  
+        }
+
+        return false;  
+    }
+
+    public void printWords() {
+        Nodo corrente = testa;
+        while (corrente != null) {
+            System.out.println(corrente.getInfo());
+            corrente = corrente.getNext();
+        }
+    }
+
+    public boolean rimuoviPrimaParola(String p){
+        if (testa == null) {
+            return false;  
+        }
+        if(testa.getInfo().equals(p)){
+            testa = testa.getNext();
+            return true;
+        }
+        if(cercaParola(p)){
+            Nodo corrente = testa; 
+            while (corrente.getNext() != null) {  
+                if (corrente.getNext().getInfo().equals(p)) {   
+                    corrente.setNext(corrente.getNext().getNext());
+                    return true;  
+                }
+                corrente = corrente.getNext();  
+            } 
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean searchPalindrome() {
+        Nodo corrente = testa;
+        while (corrente != null) {
+            if (inverteParola(corrente.getInfo()).equals(corrente.getInfo())) {
+                return true; 
+            }
+            corrente = corrente.getNext();
+        }
+        return false; 
+    }
+
+    public int wordsWithFirstLetter(char letter) {
+        int count = 0;
+        Nodo corrente = testa;
+        while (corrente != null) {
+            if (corrente.getInfo().charAt(0) == letter) {
+                count++; 
+            }
+            corrente = corrente.getNext();
+        }
+        return count;
+    }
+    
+    
+
+    public String inverteParola(String p) {
+        String inverse = "";
+        for (int i = 0; i < p.length(); i++) {
+            inverse = p.charAt(i) + inverse;
+        }
+        return inverse;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Nodo corrente = testa;
+        
+        while (corrente != null) {
+            sb.append(corrente.getInfo()).append(" ");
+            corrente = corrente.getNext();
+        }
+        
+        return sb.toString(); 
+    }
+    
+}
---
+++ es24/Main.java
@@ -0,0 +1,83 @@
+package es24;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Lista lista = new Lista();
+        int scelta = 0;
+        Scanner in = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("######  MENU #####\r\n" + 
+                "Gestione lista parole, scegliere l'operazione da eseguire:\r\n" + 
+                "1) Inserisci una nuova parola nella lista\r\n" + 
+                "2) Cancella la parola in lista\r\n" + 
+                "3) Stampa tutto il contenuto della lista\r\n" + 
+                "4) Verifica se esiste una parola palidroma di una parola inserita\r\n" + 
+                "5) Verifica se esistono piu di due parole palidrome\r\n" + 
+                "6) Restituisce la quantita di parola che inziano con la lettera inserita\r\n" + 
+                "0)Termina il programma");
+            System.out.println("Inserisci la tua scelta: ");
+            
+            try {
+                scelta = in.nextInt();
+                in.nextLine();  
+            } catch (Exception e) {
+                System.out.println("\nErrore! (Inserisci un numero da 0 a 6)\n");
+                in.nextLine();
+                continue; 
+            }
+
+            switch (scelta) {
+                case 1:
+                    System.out.println("Inserisci la parola da inserire: ");
+                    String parola = in.nextLine();
+                    if(lista.addWord(parola)){
+                        System.out.println("\nLa parola e' stata inserita con successo. \n" );
+                    } else {
+                        System.out.println("\nErrore, la parola potrebbe essere gia' stata inserita. \n" );
+                    }
+                    break;
+                case 2:
+                System.out.println("Inserisci la parola da rimuovere: ");
+                String pRimuovo = in.nextLine();
+                    if (lista.rimuoviPrimaParola(pRimuovo)) {
+                        System.out.println("La prima parola e' stata rimossa dalla lista.");
+                    } else {
+                        System.out.println("La lista è vuota o la parola inserita non e' presente nella lista.");
+                    }
+                    break;
+                case 3: 
+                    System.out.println("Contenuto della lista:");
+                    lista.printWords();
+                    break;
+                case 4:
+                    if(searchPalisrome()){
+                        System.out.println("Esiste una parola palidroma");
+                    }else{
+                        System.out.println("Non esiste una parola palidroma");
+
+                    }
+                    break;
+                case 5:
+                    
+                    break;
+                case 6: 
+                    System.out.println("Inserisci la lettera da cercare: ");
+                    char lettera = in.nextLine().charAt(0);
+                    int count = lista.wordsWithFirstLetter(lettera);
+                    System.out.println("Ci sono " + count + " parole che iniziano con la lettera " + lettera + ".");
+                    
+                    break;
+                case 0: 
+                    System.out.println("Programma terminato.");
+                    in.close(); 
+                    return; 
+                default:
+                    System.out.println("Scelta non valida.");
+                    break;
+            }
+        }
+    }
+}
