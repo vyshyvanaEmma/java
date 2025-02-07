fb361e070f6394dc5e0911ccb64b21694ab0ee0f
Benncio09 <lorebencini13@outlook.com>
2025-02-07 11:24:48 +0300

    es25

---
+++ es25/Libro.java
@@ -0,0 +1,43 @@
+
+abstract class Libro{
+    private Autore autore;
+    private String titolo;
+    private String isbn;
+    private int annoPubblicazione;
+    private String casaEditrice;
+    private double prezzoDiVendita;
+
+    public Libro(String nome, String cognome, String codiceFiscale, String titolo, String isbn, int annoPubblicazione, String casaEditrice, double prezzoDiVendita) {
+        this.autore = new Autore(nome, cognome, codiceFiscale);
+        this.titolo = titolo;
+        this.isbn = isbn;
+        this.annoPubblicazione = annoPubblicazione;
+        this.casaEditrice = casaEditrice;
+        this.prezzoDiVendita = prezzoDiVendita;
+    }
+
+    public Autore getAutore() {
+        return autore;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public int getAnnoPubblicazione() {
+        return annoPubblicazione;
+    }
+
+    public String getCasaEditrice() {
+        return casaEditrice;
+    }
+
+    public double getPrezzoDiVendita() {
+        return prezzoDiVendita;
+    }
+
+}
---
+++ es25/Pila.java
@@ -0,0 +1,15 @@
+public interface Coda {
+    void coda();
+
+    void accoda(Object info);
+
+    Object estrai();
+
+    Object first();
+
+    Object last();
+
+    Boolean isEmpty();
+
+    void clear();
+}
