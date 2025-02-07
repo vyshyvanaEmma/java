c61bd0bfe15e35b770855ae6148a38373a2a9c6b
Benncio09 <lorebencini13@outlook.com>
2025-02-07 11:50:54 +0300

    es26

---
+++ es26/Nodo.java
@@ -0,0 +1,23 @@
+public class Nodo {
+    private String info;
+    private Nodo next;
+
+    public Nodo(String info) {
+        this.info = info;
+        this.next = null;
+    }
+    
+    public String getInfo() {
+        return info;
+    }
+    public void setInfo(String info) {
+        this.info = info;
+    }
+    public Nodo getNext() {
+        return next;
+    }
+    public void setNext(Nodo next) {
+        this.next = next;
+    }
+
+}
